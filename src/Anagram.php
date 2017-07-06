35683ca069547ec9187fa33489f81c5e76814ec9
Brittany Kerr and Calla Rudolph <calla@>
2017-07-06 14:57:26 -0700

    write first test with failure

---
+++ src/Anagram.php
@@ -0,0 +1,9 @@
+<?php
+    class Anagram
+    {
+        function anagramChecker($base_word, $input1, $input2, $input3)
+        {
+
+        }
+    }
+?>
---
+++ tests/AnagramTest.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "src/Anagram.php";
+
+    class AnagramTest extends PHPUnit_Framework_TestCase
+    {
+        function testAnagramChecker()
+        {
+            $test_anagram = new Anagram;
+            $base_word = "ab";
+            $input1 = "ab";
+            $input2 = "cd";
+            $input3 = "th";
+
+            $result = $test_anagram->anagramChecker($base_word, $input1, $input2, $input3);
+
+            $this->assertEquals([$input1 => true, $input2 => false, $input3 => false], $result);
+        }
+    }
+?>
