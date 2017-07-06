5ad92468d1ad31d46af31d7532091e7b0a4d317f
Brittany Kerr and Calla Rudolph <calla@>
2017-07-06 15:56:26 -0700

    create twigs with for loops and branching

---
+++ app/app.php
@@ -0,0 +1,23 @@
+<?php
+    date_default_timezone_set("America/Los_Angeles");
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Anagram.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__."/../views"
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.html.twig');
+    });
+
+    $app->get("/result", function() use ($app) {
+        $my_input = new Anagram;
+        $my_anagram = $my_input->anagramChecker($_GET['base_word'], $_GET['input1'], $_GET['input2'], $_GET['input3']);
+        return $app['twig']->render('results.html.twig', array('results' => $my_anagram));
+    });
+
+    return $app;
+?>
